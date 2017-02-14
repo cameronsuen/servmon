ff21f80656b4d0eb827c0990a877f21e3bad0789
Cameron Suen <cameron10913@gmail.com>
2017-02-14 21:27:12 +0800

    Added MongoDB Support

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask', 'Flask-Script', 'pytest', 'sphinx', 'sphinxcontrib-httpdomain']
+    install_requires=['Flask', 'Flask-Script', 'pymongo', 'pytest', 'sphinx', 'sphinxcontrib-httpdomain']
 )
