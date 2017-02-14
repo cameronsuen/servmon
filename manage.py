6aec1eeb54aad7cd8e9ccc2d2b24325972ae8da8
Cameron Suen <cameron10913@gmail.com>
2017-02-14 22:53:41 +0800

    Added migrations utilities

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 
 from flask_script import Manager
 from servmon import app
+from migrations import machine_data_seed
 import subprocess
 
 # Enable debugging mode for development purposes
@@ -9,15 +10,24 @@ app.config['DEBUG'] = True
 
 manager = Manager(app)
 
+
 @manager.command
 def makedoc():
     """Generate HTML documentation for APIs and common utils in docs/_build/html"""
     subprocess.run(['make', '-C', 'docs', 'html'])
 
+
+@manager.command
+def migrate():
+    """Seed the MongoDB database with test data"""
+    machine_data_seed.up()
+
+
 @manager.command
 def test():
     """Execute test cases"""
     subprocess.run('pytest')
+
 
 if __name__ == '__main__':
     manager.run()
---
+++ migrations/machine_data_seed.py
@@ -0,0 +1,43 @@
+""" A Seed File to seed the Mongo DB with Test Data """
+
+# Import pymongo module to allow operations on mongo
+from pymongo import MongoClient
+
+
+def up():
+    """ Seed the database with test data """
+
+    # Mandatory for every mongodb call
+    client = MongoClient()
+    # Select which database to use
+    db = client.test_database
+    # Select which collection to use, each collection stores data of one machine
+    collection = db.sample_machine_collection
+
+    # Set up the data
+    sample_machine_state = {
+        'seeded': True,
+        'used': 200,
+        'total': 1000,
+        'partitions': [{'filesystem': '/dev/sda1', 'mountpoint': '/',
+                        'used': 100, 'subtotal': 500},
+                       {'filesystem': '/dev/sda2', 'mountpoint': '/var',
+                        'used': 100, 'subtotal': 500}]
+    }
+
+    # Actual insertion of data
+    collection.insert_one(sample_machine_state)
+
+
+def down():
+    """ Remove the seeded test data """
+
+    # Mandatory for every mongodb call
+    client = MongoClient()
+    # Select which database to use
+    db = client.test_database
+    # Select which collection to use, each collection stores data of one machine
+    collection = db.sample_machine_collection
+
+    # Remove the data having attribute seeded = True
+    collection.delete_many({'seeded': True})
